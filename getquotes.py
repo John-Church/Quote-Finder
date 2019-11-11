6ac0f35fbd0d58087082e3ca6464fc5a0f3acd90
John-Church <jchurch21@students.claremontmckenna.edu>
2019-11-11 00:29:29 -0800

    Clean up code

--- getquotes.py
+++ getquotes.py
@@ -4,24 +4,30 @@ import threading
 from collections import Counter
 
 def textGetter(link):
+    '''
+    Returns all the text from a wepage of a given link.
+    '''
     r = requests.get(link)
+    # grabs web content as BeautifulSoup object
     soup = BeautifulSoup(r.content, 'html.parser')
-    return soup.get_text()
+    return soup.get_text()  # returns all text from web content
 
-file = open("Quotes.txt", "w")
-link = "http://fortunes.herokuapp.com/random/raw"
+file = open("Quotes.txt", "w")  # opens/ creates new text file to record quotes
+link = "http://fortunes.herokuapp.com/random/raw"   # link to parse
 text1 = textGetter(link)
 quoteNum = 0
-quoteList = Counter()
+quoteList = Counter()   # python counter (dict subclass that counts # of times
+                        # an item has been recorded)
 
 def recordQuotes():
+    # Globals needed for threading
     global text1
     global quoteNum
     global link
     global quoteList
-    while (quoteList[text1] == 0):
+    while (quoteList[text1] == 0): # iterate while quote hasn't been counted yet
         file.write(text1 + "\n\n")
-        quoteList[text1] += 1
+        quoteList[text1] += 1 # count quote
         print("So far, {} Quotes have been recorded.".format(quoteNum))
         text1 = textGetter(link);
         quoteNum += 1
